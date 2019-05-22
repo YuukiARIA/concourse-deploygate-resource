bb864b11f25f563a12c87c31527567d4c67c028b
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2019-05-22 14:47:06 +0900

    Split deploygate models

--- deploygate/upload.go
+++ deploygate/upload.go
@@ -13,33 +13,6 @@ import (
 	"strconv"
 )
 
-type Results struct {
-	Name        string `json:"name"`
-	PackageName string `json:"package_name"`
-	OSName      string `json:"os_name"`
-	Path        string `json:"path"`
-	Revision    int    `json:"revision"`
-	VersionCode int    `json:"version_code"`
-	VersionName string `json:"version_name"`
-	Message     string `json:"message"`
-}
-
-type ErrorResponse struct {
-	Error   bool   `json:"error"`
-	Message string `json:"message"`
-	Because string `json:"because"`
-}
-
-type SuccessResponse struct {
-	Error   bool    `json:"error"`
-	Results Results `json:"results"`
-}
-
-type Response struct {
-	SuccessResponse *SuccessResponse
-	ErrorResponse   *ErrorResponse
-}
-
 func (r *Response) IsSuccess() bool {
 	return r.SuccessResponse != nil
 }
