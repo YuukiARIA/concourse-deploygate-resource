c8beb97c2b5e387e26a1b035329b0aca9aac4973
YuukiARIA <yuuki.aria.del.viscontie@gmail.com>
2019-05-29 20:58:10 +0900

    Implement GET single app

--- deploygate/apps.go
+++ deploygate/apps.go
@@ -6,15 +6,20 @@ import (
 	"net/http"
 )
 
+type ResponseBase struct {
+	Error   bool   `json:"error"`
+	Message string `json:"message,omitempty"`
+	Because string `json:"because,omitempty"`
+}
+
 type GetAppsResponse struct {
-	Error bool `json:"error"`
-
-	// Success
+	ResponseBase
 	Apps []App `json:"applications"`
+}
 
-	// Error
-	Message string `json:"message"`
-	Because string `json:"because"`
+type GetAppResponse struct {
+	ResponseBase
+	App App `json:"application"`
 }
 
 func (c *Client) GetApps(organization string) (*GetAppsResponse, error) {
@@ -33,6 +38,28 @@ func (c *Client) GetApps(organization string) (*GetAppsResponse, error) {
 	defer res.Body.Close()
 
 	resObj := new(GetAppsResponse)
+	if err := json.NewDecoder(res.Body).Decode(resObj); err != nil {
+		return nil, err
+	}
+	return resObj, nil
+}
+
+func (c *Client) GetApp(organization, platform, packageName string) (*GetAppResponse, error) {
+	url := fmt.Sprintf("https://deploygate.com/api/organizations/%s/platforms/%s/apps/%s", organization, platform, packageName)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Authorization", c.ApiKey)
+
+	httpClient := http.Client{}
+	res, err := httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	resObj := new(GetAppResponse)
 	if err := json.NewDecoder(res.Body).Decode(resObj); err != nil {
 		return nil, err
 	}
